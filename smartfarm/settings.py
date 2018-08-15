55f8c56859539bf3e81e48b375bd8163c6e4ea43
Ben_Mbiyu <benedictmbiyuthangwa@gmail.com>
2018-08-15 17:56:23 +0300

    Send activation email to user and activation link for their accounts

--- smartfarm/settings.py
+++ smartfarm/settings.py
@@ -74,7 +74,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'smartfarm.wsgi.application'
-
+# Email configuration
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
