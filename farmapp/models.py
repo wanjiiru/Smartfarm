0f84138432dc14aa0715715fe3fd496a3ab20ce4
Ben_Mbiyu <benedictmbiyuthangwa@gmail.com>
2018-08-15 18:16:15 +0300

    Link every user instance to a profile

--- farmapp/models.py
+++ farmapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    email = models.EmailField(max_length=254,null=True)
+
+    def __str__(cls):
+        return cls.user
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
