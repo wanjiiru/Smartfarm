1b87b9c41296ce408c5588ea08f6239c93672315
wanjiiru <liz240@live.com>
2018-08-21 22:00:19 +0300

    add ui

--- farmapp/urls.py
+++ farmapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^image/$', views.add_image, name='upload_image'),
+    url(r'^how/$', views.how_it_works, name='how'),
+
 ]
--- farmapp/views.py
+++ farmapp/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Image
+from .forms import ImageForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+def add_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            add=form.save(commit=False)
+            add.owner = current_user
+            add.save()
+            return redirect('index')
+    else:
+        form = ImageForm()
+
+
+    return render(request,'image.html',locals())
+
+def how_it_works(request):
+
+
+    return render(request,'how.html', locals())
