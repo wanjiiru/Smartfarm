b489b305da1f83041d904803401e6c2f0e3e77b4
Ben_Mbiyu <benedictmbiyuthangwa@gmail.com>
2018-08-22 11:38:48 +0300

    Create a user form and a user profile form

---
+++ farmapp/forms.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect,HttpResponseRedirect
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from farmapp.models import Profile
+from django import forms
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio','location')
