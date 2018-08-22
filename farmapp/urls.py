6bff2e7f1ee323e1949c0ec90d630e1dd3783543
Ben_Mbiyu <benedictmbiyuthangwa@gmail.com>
2018-08-22 11:42:03 +0300

    Include url's for google authentication sign in

--- farmapp/urls.py
+++ farmapp/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^accounts/', include('social_django.urls', namespace='social')),
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^profile/$', views.update_profile, name="update_profile"),
 ]
