197bc92c6566d9ce2a6a5c55d5a277e9b1c37415
wanjiiru <liz240@live.com>
2018-08-22 12:30:44 +0300

    add results and how templates, merge with xavier

--- farmapp/views.py
+++ farmapp/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Image
+from .brain.brain import recognise
 from .forms import ImageForm
+from base64 import b64encode
+from io import BytesIO
 
 # Create your views here.
 def index(request):
@@ -15,10 +18,15 @@ def add_image(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
-            add=form.save(commit=False)
-            add.owner = current_user
-            add.save()
-            return redirect('index')
+            # add=form.save(commit=False)
+            # add.owner = current_user
+            image = next(request.FILES.values()).read()
+            results = recognise(BytesIO(image))
+
+            buffer = b64encode(image)
+
+            # add.save()
+            return render(request, 'results.html', locals())
     else:
         form = ImageForm()
 
@@ -26,6 +34,5 @@ def add_image(request):
     return render(request,'image.html',locals())
 
 def how_it_works(request):
-
 
     return render(request,'how.html', locals())
