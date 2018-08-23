ddc4820bad46bae66203986f21c032fa3b27eeb0
wanjiiru <liz240@live.com>
2018-08-23 14:24:51 +0300

    merge with development

--- farmapp/forms.py
+++ farmapp/forms.py
@@ -4,12 +4,4 @@ from .models import Image
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-<<<<<<< HEAD
         exclude =['posted']
-
-
-
-
-=======
-        exclude =['owner', 'posted']
->>>>>>> 15d4059dc6d5eb49bb9ef5624aab89147b96173a
