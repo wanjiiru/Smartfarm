2d51ab354a197a6f8c2b8ed61c9abb0b4ee78624
xv <xavierkibet@gmail.com>
2018-09-03 11:45:39 +0300

    remw

--- farmapp/models.py
+++ farmapp/models.py
@@ -1,11 +1,10 @@
 from django.contrib.gis.db import models
 from django.contrib.auth.models import User
+# from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import datetime as dt
 from django.contrib.postgres.fields import ArrayField
-
-
 
 
 # Create your models here.
@@ -14,15 +13,16 @@ class Profile(models.Model):
     email = models.EmailField(max_length=254,null=True)
     bio = models.TextField(max_length=500,blank=True)
     location = models.CharField(max_length=30,blank=True)
- 
 
     def __str__(cls):
         return cls.user
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
