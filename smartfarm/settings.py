24dead351c9ab91cb09f9c001bb131f444fb6ac0
xv <xavierkibet@gmail.com>
2018-09-05 18:57:01 +0300

    implemented live location retrieval

--- smartfarm/settings.py
+++ smartfarm/settings.py
@@ -137,7 +137,6 @@ if config('MODE')=="dev":
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
-           'PORT': '',
        }
 
    }
